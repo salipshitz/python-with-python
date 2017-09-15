cfdadfbf8477a676360ccd31fb1a9c07cc1acc96
Sa'ar <geger40.1@gmail.com>
2017-09-15 14:20:41 -0700

    Update and rename main.py to window.py

---
+++ window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from screens import *
+
+class Main(Frame):
+	def __init__(self, master=None):
+		super(Main, self).__init__()
+		self.master = master
+		self.master.title("Learn Python With Python")
+		Window.s0()
+		
