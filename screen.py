de5d8dfb91d0485c21f78fc8894c9d6a4db35635
Sa'ar <geger40.1@gmail.com>
2017-09-16 10:13:38 -0700

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,11 +1,13 @@
 from tkinter import *
 from windows import *
+from UIHandler import ImageView
 
 class Screen(Windows):
 	def __init__(self, master=None):
 		super(Screen, self).__init__(master)
 		self.master = master
 		self.master.title("Learn Python With Python")
+		self.logoview = ImageView(master).pack()
 		self.pack(fill=BOTH, expand=1)
 		self.s0()
 		
