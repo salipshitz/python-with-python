514e34bcc6095316f36d5b02fab6e632de67cc10
Sa'ar <geger40.1@gmail.com>
2017-09-16 09:37:23 -0700

    Update windows.py

--- windows.py
+++ windows.py
@@ -2,11 +2,14 @@ from tkinter import *
 from abc import ABC
 
 class Windows(Frame, ABC):
+        def __init__(self, master=None):
+               super(Windows, self).__init__()
+        
         def close_app(self):
                 self.master.destroy()
                 print("Succesfully killed")
                 
-        def cenBtn(self):
+        def cenBtn(self, txt, func):
                b = Button(self, text=txt, command=func)
                b.pack()
                 
@@ -16,16 +19,27 @@ class Windows(Frame, ABC):
                 return btn
                 
         def btnQuit(self):
-                self.choBtn("Quit", self.close_app, 0, 0)
+                return self.choBtn("Quit", self.close_app, 0, 0)
+                
+        def choLbl(self, txt, x, y):
+                lbl = Label(self, text=txt)
+                lbl.place(x=x, y=y)
+                return lbl
+        
+        def cenLbl(self, txt):
+                return Label(self, text=txt).pack()
                 
         def new(self):
                 for widget in self.winfo_children():
                         widget.destroy()
                 self.btnQuit()
         
-        def __init__(self, master=None):
-               super(Windows, self).__init__()
+        
         
         def s0(self):
                 self.new()
-		
+                self.cenLbl("Hello. Welcome to LearningPythonWithPython")
+                self.cenBtn("Lesson 1: Setting up the IDE", s1)
+        
+        def s1(self):
+                
