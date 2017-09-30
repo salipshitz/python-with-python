b113e5657380582aa48d0363aecda701d4448d9a
Sa'ar <geger40.1@gmail.com>
2017-09-30 13:50:39 -0700

    Delete comments.py

--- comments.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-import sqlite3
-
-class Comments:
-        def __init__(self):
-                login = sqlite3.connect("login.db")
-                comments = sqlite3.connect("comments.db")
