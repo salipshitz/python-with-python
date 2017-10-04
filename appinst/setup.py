f48202e7b4e30a50c4209784618d4ac5f40c9456
Sa'ar <geger40.1@gmail.com>
2017-10-03 17:42:51 -0700

    Update setup.py

--- appinst/setup.py
+++ appinst/setup.py
@@ -3,11 +3,8 @@ import py2exe
 import windows, screen
 
 setup(
-    options = {'py2exe': {'bundle_files': 1}}
-    zipFile = None
     windows = [{
-        "script": "mainloop",
-        "icon_resources": [(1, "pycon-icon.ico")]
+        "script": "mainloop.py",
         "dest_base": "LearnPythonWithPython"
     }]
 )
